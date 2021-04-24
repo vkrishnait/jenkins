cf942aecd6c826617e22f3015d3cf3f7de7f15b2
I304224 <krishna.venkatesan@sap.com>
2021-04-24 16:33:23 +0530

    test

---
+++ t1/1.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "krishna" {
+    ami = "ami-0d758c1134823146a"
+     instance_type = "t2.micro"
+     tags = {
+       "Name" = "VM1"
+     }
+
+}
