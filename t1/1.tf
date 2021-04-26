5a3a8d51200002e9af3c363d6d994b6b6adb0703
vkrishnait <47443290+vkrishnait@users.noreply.github.com>
2021-04-26 08:11:21 +0530

    Update 1.tf

--- t1/1.tf
+++ t1/1.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "EC" {
+resource "aws_instance" "EC1" {
     ami = "ami-0d758c1134823146a"
      instance_type = "t2.micro"
      tags = {
