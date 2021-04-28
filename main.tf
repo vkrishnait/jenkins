ece1b09c585b731673e21c4735f5f66af7b9b0a0
I304224 <krishna.venkatesan@sap.com>
2021-04-28 10:49:57 +0530

    add key

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "krishna" {
     ami = "ami-0d758c1134823146a"
      instance_type = "t2.micro"
+     key_name      = "var.ssh_key"
      tags = {
        "Name" = "VM1"
      }
