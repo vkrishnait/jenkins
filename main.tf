421c0962d3a8d7b12ee8d64e599abff4a73fcebb
vkrishnait <47443290+vkrishnait@users.noreply.github.com>
2021-04-28 12:22:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+data "aws_security_group" "selected" {
+  id = var.sg
+}
 
   ## ubuntu image#####
   ## my ssh_key AWS_KV ########
-
 
 resource "aws_instance" "krishna1" {
     ami = ami-0d758c1134823146a
