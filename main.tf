35c5fbb1ce34bdefb77fab38ab9f73d0f9d61a11
vkrishnait <47443290+vkrishnait@users.noreply.github.com>
2021-04-28 11:28:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+
+  ## ubuntu image#####
+  ## my ssh_key AWS_KV ########
+
+
 resource "aws_instance" "krishna1" {
     ami = "ami-0d758c1134823146a"
      instance_type = "t2.micro"
@@ -9,5 +14,5 @@ resource "aws_instance" "krishna1" {
      tags = {
        "Name" = "VM8"
      }
-
+  user_data = file("postscript.sh")
 }
