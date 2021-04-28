aa3e38f0ba91fa943bad9ef556f79c32df7f5102
vkrishnait <47443290+vkrishnait@users.noreply.github.com>
2021-04-28 12:04:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,10 @@ resource "aws_security_group" "JenkinSG" {
 
 
 resource "aws_instance" "krishna1" {
-    ami = "ami-0d758c1134823146a"
-     instance_type = "t2.micro"
+    ami = ami-0d758c1134823146a
+     instance_type = t2.micro
      key_name = var.ssh_key
+     security_groups = Jenkin SG1
      tags = {
        "Name" = "VM8"
      }
